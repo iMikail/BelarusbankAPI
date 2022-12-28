bee45d2ffd64a76eb3de25017b1c4fe0ec0f5429
Mikhail <imisha.volkov@gmail.com>
2022-12-28 11:56:13 +0300

    Initial Commit

---
+++ task4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task4
+//
+//  Created by Misha Volkov on 28.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
